d01788f2627c8d43cb16fe17fbe3e89cc5baea12
Xrat Zhou <xratzh@gmail.com>
2021-03-14 00:05:05 +0800

    Update D9.sh

--- D9.sh
+++ D9.sh
@@ -4,7 +4,7 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 [ "$EUID" -ne '0' ] && echo "Error,This script must be run as root! " && exit 1
-apt install linux-image-4.9.0-4-amd64
+wget --no-check-certificate -qO '/tmp/linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb' 'http://snapshot.debian.org/archive/debian/20171224T175424Z/pool/main/l/linux/linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb' && dpkg -i '/tmp/linux-image-4.9.0-4-amd64_4.9.65-3+deb9u1_amd64.deb'
 
 KernelList="$(dpkg -l |grep 'linux-image' |awk '{print $2}')"
 [ -z "$(echo $KernelList |grep -o linux-image-4.9.0-4-amd64)" ] && echo "Install error." && exit 1
